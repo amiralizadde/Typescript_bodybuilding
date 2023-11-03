c1ce11ad3f1129e99da70afcc56eb48ee5925423
amiralizadde <75627484+amiralizadde@users.noreply.github.com>
2023-11-03 13:31:57 +0330

    completed navabr

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
 import Navbar from './components/header/Navbar.tsx'
 import { SelectedPage } from './shared/types.ts'
@@ -7,11 +7,28 @@ import { SelectedPage } from './shared/types.ts'
 function App() {
 
   const [selectedPage  , setSelectedPage] = useState<SelectedPage>(SelectedPage.Home)
+  const [isTopOfPage , setIsTopOfPage] = useState<boolean>(false)
+
+  const handelScroll = ()=>{
+    window.scrollY === 0 && (
+      setIsTopOfPage(true),
+      setSelectedPage(SelectedPage.Home)
+    )
+    window.scrollY !== 0 && setIsTopOfPage(false)
+  }
+
+  useEffect(()=>{
+
+window.addEventListener('scroll' , handelScroll)
+return ()=>window.removeEventListener('scroll' , handelScroll)
+
+  },[])
 
   return (
     <>
        <div className='bg-gray-20 '>
           <Navbar 
+          isTopOfPage={isTopOfPage}
           selectedPage={selectedPage}
           setSelectedPage={setSelectedPage}
           />
