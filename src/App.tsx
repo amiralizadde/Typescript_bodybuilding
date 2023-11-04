97033fbcf1f1e218455d0036f0c814e3fe97ffd0
amiralizadde <75627484+amiralizadde@users.noreply.github.com>
2023-11-05 00:43:31 +0330

    add benitits and home section

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,41 @@
-import { useEffect, useState } from 'react'
-import './App.css'
-import Navbar from './components/header/Navbar.tsx'
-import { SelectedPage } from './shared/types.ts'
-
+import { useEffect, useState } from "react";
+import "./App.css";
+import Navbar from "./components/header/Navbar.tsx";
+import { SelectedPage } from "./shared/types.ts";
+import Home from "./components/home/Home";
+import Benefits from "./components/benefits/Benefits";
 
 function App() {
+  const [selectedPage, setSelectedPage] = useState<SelectedPage>(
+    SelectedPage.Home
+  );
+  const [isTopOfPage, setIsTopOfPage] = useState<boolean>(false);
 
-  const [selectedPage  , setSelectedPage] = useState<SelectedPage>(SelectedPage.Home)
-  const [isTopOfPage , setIsTopOfPage] = useState<boolean>(false)
+  const handelScroll = () => {
+    window.scrollY === 0 &&
+      (setIsTopOfPage(true), setSelectedPage(SelectedPage.Home));
+    window.scrollY !== 0 && setIsTopOfPage(false);
+  };
 
-  const handelScroll = ()=>{
-    window.scrollY === 0 && (
-      setIsTopOfPage(true),
-      setSelectedPage(SelectedPage.Home)
-    )
-    window.scrollY !== 0 && setIsTopOfPage(false)
-  }
-
-  useEffect(()=>{
-
-window.addEventListener('scroll' , handelScroll)
-return ()=>window.removeEventListener('scroll' , handelScroll)
-
-  },[])
+  useEffect(() => {
+    window.addEventListener("scroll", handelScroll);
+    return () => window.removeEventListener("scroll", handelScroll);
+  }, []);
 
   return (
     <>
-       <div className='bg-gray-20 '>
-          <Navbar 
+      <div className="app bg-gray-20 ">
+        <Navbar
           isTopOfPage={isTopOfPage}
           selectedPage={selectedPage}
           setSelectedPage={setSelectedPage}
-          />
-       </div>
+        />
+
+        <Home setSelectedPage={setSelectedPage} />
+        <Benefits setSelectedPage={setSelectedPage} />
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
